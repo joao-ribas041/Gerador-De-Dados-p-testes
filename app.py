e752c3ea8f664d746f6f0daa0d70da69b29364c1
João Ribas <ribas.joao1410@gmail.com>
2022-10-26 22:18:13 -0300

    Versao Base

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+from unicodedata import name
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Olá Mundo'
+    return render_template('index.html', name=name)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000, host='localhost', debug=True)
